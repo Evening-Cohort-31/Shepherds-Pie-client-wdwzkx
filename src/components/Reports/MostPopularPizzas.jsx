3dcc9eaefa719f864678d60c5778889f56960708
Lea Edwards <leaxedw@gmail.com>
2025-11-05 21:28:30 -0600

    adding in arrays for the options

--- src/components/Reports/MostPopularPizzas.jsx
+++ src/components/Reports/MostPopularPizzas.jsx
@@ -1,1 +1,9 @@
 import "./MostPopularPizzas.css";
+
+import { useState, useEffect } from react
+import { getReports } from "../../services/reportService";
+
+export const PieChart = () => {
+    const [salesData, setSalesData] = useState([])
+
+}
