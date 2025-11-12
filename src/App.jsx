e0dccc44f7a492b18c4bd15d723468ed93575eb3
Dale Hobbs <dale.hobbs.dev@gmail.com>
2025-11-11 21:47:05 -0600

    resolved merge conflicts with develop update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import BootstrapSampler from "./components/Bootstrap/BootstrapSampler.jsx";
 import { PizzaOrderBuilder } from "./components/Pizzas/PizzaOrderBuilder.jsx";
+import { EmployeeList } from "./components/employees/EmployeeList.jsx";
 
 export const App = () => {
   return (
     <>
       {/* <PizzaOrderBuilder /> */}
-      <BootstrapSampler />
+      {/* <BootstrapSampler /> */}
+      <EmployeeList />
     </>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -8,7 +8,6 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <EmployeeList />
+    <App />
   </React.StrictMode>
 );
