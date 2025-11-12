04a2e17809f2967ba70acef96f5cdf86b81da504
Erin Telfer <eringtelfer@gmail.com>
2025-11-11 22:41:38 -0600

    created order list and started to build todaysorders filter. next step is to make that display as default and add bootstrap css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import BootstrapSampler from "./components/Bootstrap/BootstrapSampler.jsx";
+import { OrderList } from "./components/orders/OrderList.jsx";
 import { PizzaOrderBuilder } from "./components/Pizzas/PizzaOrderBuilder.jsx";
 
 export const App = () => {
@@ -7,6 +8,7 @@ export const App = () => {
     <>
       {/* <PizzaOrderBuilder /> */}
       <BootstrapSampler />
+      <OrderList />
     </>
   );
 };
--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,1 +1,6 @@
 import { API_BASE_URL, postJson, fetchJson } from "./apiSettings";
+
+
+export const getOrders = () => {
+    return fetchJson("/orders?_expand=customer")
+}
